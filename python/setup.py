44d364cd4eb014a35889be047353f39186a324a7
Johannes Schrimpf <johannes.schrimpf@blueye.no>
2018-07-11 15:46:40 +0200

    Bump version

--- python/setup.py
+++ python/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 from setuptools import find_packages
 setup(name='p2_app_protocol',
-      version='1.0.1',
+      version='1.0.2',
       author='Johannes Schrimpf',
       author_email='johannes.schrimpf@blueye.no',
       url='https://github.com/BluEye-Robotics/p2_app_protocol',
