71a39598ad243283f995531133a740fe6f343924
Jonas Follesø <jonas@follesoe.no>
2018-07-11 13:57:48 +0200

    Add README and author info

---
+++ python/README.txt
@@ -0,0 +1,3 @@
+p2_app_protocol Python Package
+
+This package provides a Python definition of the P2 app protocol.
--- python/setup.py
+++ python/setup.py
@@ -2,6 +2,8 @@ from distutils.core import setup
 from setuptools import find_packages
 setup(name='p2_app_protocol',
       version='1.0',
+      author='Johannes Schrimpf',
+      author_email='johannes.schrimpf@blueye.no',
       package_dir= {'p2_app_protocol': 'p2_app_protocol'},
       package_data={'': ['data/protocol.json']},
       packages=find_packages('.'),
