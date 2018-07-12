589117cfcfdd8f4c1613d4d5413bd5053e6fda8d
Jonas Follesø <jonas@follesoe.no>
2018-07-12 12:52:33 +0200

    Bump version number and skip file copy on install

--- python/setup.py
+++ python/setup.py
@@ -1,11 +1,16 @@
 import os
+import sys
 import shutil
 from distutils.core import setup
 from setuptools import find_packages
 
-shutil.copy('../protocol.json', 'p2_app_protocol/data/protocol.json')
+print(sys.argv)
+
+if sys.argv[1] != "install":
+    shutil.copy('../protocol.json', 'p2_app_protocol/data/protocol.json')
+
 setup(name='p2_app_protocol',
-      version='1.0.4',
+      version='1.0.5',
       author='Johannes Schrimpf',
       author_email='johannes.schrimpf@blueye.no',
       url='https://github.com/BluEye-Robotics/p2_app_protocol',
