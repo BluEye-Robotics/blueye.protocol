ad74b4a416f3f612a6310a733769e8487a91218d
Jonas Follesø <jonas@follesoe.no>
2018-07-12 12:59:23 +0200

    Only copy on sdist command

--- python/setup.py
+++ python/setup.py
@@ -6,11 +6,12 @@ from setuptools import find_packages
 
 print(sys.argv)
 
-if sys.argv[1] != "install":
+# Only copy the protocol file when building distribution package
+if "sdist" in sys.argv[1]:
     shutil.copy('../protocol.json', 'p2_app_protocol/data/protocol.json')
 
 setup(name='p2_app_protocol',
-      version='1.0.5',
+      version='1.0.6',
       author='Johannes Schrimpf',
       author_email='johannes.schrimpf@blueye.no',
       url='https://github.com/BluEye-Robotics/p2_app_protocol',
