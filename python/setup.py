9d277a1ea79de04f3d3b72e1fd22264d6f5d3325
Jonas Follesø <jonas@follesoe.no>
2018-07-12 11:23:03 +0200

    Copy protocol.json in setup.py and remove symlink

--- python/setup.py
+++ python/setup.py
@@ -1,11 +1,15 @@
+import os
+import shutil
 from distutils.core import setup
 from setuptools import find_packages
+
+shutil.copy('../protocol.json', 'p2_app_protocol/data/protocol.json')
 setup(name='p2_app_protocol',
-      version='1.0.2',
+      version='1.0.3',
       author='Johannes Schrimpf',
       author_email='johannes.schrimpf@blueye.no',
       url='https://github.com/BluEye-Robotics/p2_app_protocol',
       package_dir= {'p2_app_protocol': 'p2_app_protocol'},
-      package_data={'': ['data/protocol.json']},
+      package_data= {'p2_app_protocol': ['data/protocol.json']},
       packages=find_packages('.'),
-      )
+)
