af495ffe32d33a29c587812a83ab41c38762cf76
Aksel Lenes <aksel.lenes@gmail.com>
2019-07-24 09:48:29 +0200

    Don't use dataclasses
    They provide little value for this application, see discussion in
    https://github.com/BluEye-Robotics/p2_app_protocol/pull/7#discussion_r306342464

--- python/p2_app_protocol/drone_commands.py
+++ python/p2_app_protocol/drone_commands.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import struct
-from dataclasses import dataclass
 
 class PioneerCommand:
     @property
@@ -9,15 +8,13 @@ class PioneerCommand:
         format_string = 'c' + self._format_string
         return struct.pack(format_string, self._cmd_type.encode('utf-8'), *self._data)
 
-@dataclass
 class LightCommand(PioneerCommand):
     """
     brightness_upper: uint8 , 0 = lights off, 255 = max lights
     brightness_lower: uint8 , 0 = lights off, 255 = max lights
     """
-    brightness_upper: int
-    brightness_lower: int
-    def __post_init__(self):
-        self._data = (self.brightness_upper, self.brightness_lower)
+    def __init__(self, brightness_upper, brightness_lower):
+        self._data = (brightness_upper, brightness_lower)
         self._format_string = 'BB'
         self._cmd_type = 'l'
+
