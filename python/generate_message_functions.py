75e6c55a7e8e2a9591540851f35ab32b090d535f
Aksel Lenes <aksel.lenes@gmail.com>
2019-08-09 12:46:05 +0200

    Format code with Black code formatter

--- python/generate_message_functions.py
+++ python/generate_message_functions.py
@@ -6,22 +6,29 @@ from jinja2 import FileSystemLoader, Environment
 
 
 PATH = os.path.dirname(os.path.abspath(__file__))
-MODULE_PATH = os.path.join(PATH, 'p2_app_protocol')
-TEMPLATE_ENVIRONMENT = Environment(autoescape=False, loader=FileSystemLoader(os.path.join(MODULE_PATH, 'templates')))
+MODULE_PATH = os.path.join(PATH, "p2_app_protocol")
+TEMPLATE_ENVIRONMENT = Environment(
+    autoescape=False, loader=FileSystemLoader(os.path.join(MODULE_PATH, "templates"))
+)
+
 
 def get_protocol(protocol_path):
     with open(protocol_path) as json_file:
         jdata = json.load(json_file)
         return jdata
 
+
 def write_function():
-    tcp_protocol = get_protocol(os.path.join(MODULE_PATH, 'tcp_protocol.json'))[0]
-    file_name = os.path.join(MODULE_PATH, 'message_methods.py')
-    with open(file_name, 'w') as f:
-        for command in tcp_protocol['commands']:
-            command_type = command['command_type']
-            python_command = TEMPLATE_ENVIRONMENT.get_template('send_command_function_template.py').render(command)
+    tcp_protocol = get_protocol(os.path.join(MODULE_PATH, "tcp_protocol.json"))[0]
+    file_name = os.path.join(MODULE_PATH, "message_methods.py")
+    with open(file_name, "w") as f:
+        for command in tcp_protocol["commands"]:
+            command_type = command["command_type"]
+            python_command = TEMPLATE_ENVIRONMENT.get_template(
+                "send_command_function_template.py"
+            ).render(command)
             f.write(python_command)
+
 
 if __name__ == "__main__":
     write_function()
--- python/tests/test_tcp_client.py
+++ python/tests/test_tcp_client.py
@@ -2,9 +2,11 @@
 import pytest
 from p2_app_protocol import TcpClient
 
+
 @pytest.fixture
 def mocked_socket(mocker):
-    mocker.patch('socket.socket', autospec=True)
+    mocker.patch("socket.socket", autospec=True)
+
 
 @pytest.fixture
 def tcp_client(mocked_socket):
@@ -12,22 +14,28 @@ def tcp_client(mocked_socket):
     tc.connect()
     yield tc
 
-@pytest.mark.parametrize('function_name, expected_message', [
-    ('auto_heading_on', b'h'),
-    ('auto_heading_off', b'H'),
-    ('auto_depth_on', b'd'),
-    ('auto_depth_off', b'D'),
-])
+
+@pytest.mark.parametrize(
+    "function_name, expected_message",
+    [
+        ("auto_heading_on", b"h"),
+        ("auto_heading_off", b"H"),
+        ("auto_depth_on", b"d"),
+        ("auto_depth_off", b"D"),
+    ],
+)
 def test_commands_produce_correct_message(tcp_client, function_name, expected_message):
     func = getattr(tcp_client, function_name)
     func()
     tcp_client._sock.send.assert_called_with(expected_message)
 
-@pytest.mark.parametrize('top_lights, bottom_lights, expected_reply', [
-    (0, 0, b'l\x00\x00'),
-    (50, 50, b'l\x32\x32'),
-    (255, 255, b'l\xFF\xFF')
-])
-def test_set_light_command_produces_correct_message(tcp_client, top_lights, bottom_lights, expected_reply):
+
+@pytest.mark.parametrize(
+    "top_lights, bottom_lights, expected_reply",
+    [(0, 0, b"l\x00\x00"), (50, 50, b"l\x32\x32"), (255, 255, b"l\xFF\xFF")],
+)
+def test_set_light_command_produces_correct_message(
+    tcp_client, top_lights, bottom_lights, expected_reply
+):
     tcp_client.set_light_command(top_lights, bottom_lights)
     tcp_client._sock.send.assert_called_with(expected_reply)
