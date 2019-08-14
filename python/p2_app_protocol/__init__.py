708404cfd00ff0f32f9e5281eca4f14dbdbbc2b5
Sindre Hansen <sindrehansen92@gmail.com>
2019-08-14 16:18:31 +0200

    Remove faulty import

--- python/p2_app_protocol/__init__.py
+++ python/p2_app_protocol/__init__.py
@@ -3,4 +3,3 @@ from .protocol_data import _generator_hash, _json_hash, protocol_data
 from .tcp_client import TcpClient
 from .tcp_protocol_class import TcpCommands
 from .udp_client import UdpClient
-import .exceptions
