2936e7f5745de061e55a094d3525666b9ca59e5b
Sindre Hansen <sindrehansen92@gmail.com>
2019-08-26 16:15:26 +0200

    Rename protocol.py to udp_protocol_parser.py

--- blueye/protocol/__init__.py
+++ blueye/protocol/__init__.py
@@ -1,4 +1,4 @@
-from .protocol import AppProtocol
+from .udp_protocol_parser import AppProtocol
 from .udp_protocol_dict import _generator_hash, _json_hash, protocol_data
 from .tcp_protocol_class import TcpClient
 from .udp_client import UdpClient
