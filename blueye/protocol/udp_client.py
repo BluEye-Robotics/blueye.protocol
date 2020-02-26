19ef9e370406a2cc5b2383cfe3133e092a3ba7a2
Sindre Hansen <sindrehansen92@gmail.com>
2020-02-26 15:17:53 +0100

    Enable reuse of UDP port for MacOS

--- blueye/protocol/udp_client.py
+++ blueye/protocol/udp_client.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 
 import socket
 import time
+import platform
 
 from blueye.protocol import AppProtocol
 
@@ -17,7 +18,13 @@ class UdpClient:
 
         self._sock = socket.socket(socket.AF_INET,
                                    socket.SOCK_DGRAM)
-        self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # To able to run multiple UdpClients on MacOS one needs to set
+        # SO_REUSEPORT, however on Windows that option does not exist,
+        # so we use SO_REUSEADDR for Windows and Linux.
+        if platform.system() == "Darwin":
+            self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        else:
+            self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self._ap = AppProtocol(protocol_description)
         self.bind()
 
