59757c21f16deee48352958a30b768df43de9bc4
Johannes Schrimpf <johannes.schrimpf@blueye.no>
2021-05-07 15:40:21 +0200

    Fix tests and add timeout to tests

--- tests/test_udp_client.py
+++ tests/test_udp_client.py
@@ -5,6 +5,7 @@ import struct
 import threading
 import time
 import unittest
+import timeout_decorator
 from unittest.mock import *
 
 from blueye.protocol import UdpClient
@@ -91,30 +92,34 @@ class TestUdpClient(unittest.TestCase):
     def setUp(self):
         pass
 
+    @timeout_decorator.timeout(1)
     def test_get_raw_data(self):
         us = UDPServer()
-        uc = UdpClient(protocol_description=fake_json, port=UDP_PORT)
+        uc = UdpClient(protocol_description=fake_json, port=UDP_PORT, drone_ip="127.0.0.1")
         data = uc._get_raw_data()
         us.stop_thread()
         self.assertEqual(data, struct.pack("<BBb", 2, 2, 2))
 
+    @timeout_decorator.timeout(1)
     def test_get_data(self):
         us = UDPServer()
-        uc = UdpClient(protocol_description=fake_json, port=UDP_PORT)
+        uc = UdpClient(protocol_description=fake_json, port=UDP_PORT, drone_ip="127.0.0.1")
         data = uc.get_data()
         us.stop_thread()
         self.assertEqual(data, (2, 2, 2))
 
+    @timeout_decorator.timeout(1)
     def test_get_data_t3(self):
         us = UDPServer()
-        uc = UdpClient(protocol_description=fake_json, port=UDP_PORT)
+        uc = UdpClient(protocol_description=fake_json, port=UDP_PORT, drone_ip="127.0.0.1")
         data = uc.get_data(packet_type=3)
         us.stop_thread()
         self.assertEqual(data, (2, 3, 3))
 
+    @timeout_decorator.timeout(1)
     def test_get_data_dict(self):
         us = UDPServer()
-        uc = UdpClient(protocol_description=fake_json, port=UDP_PORT)
+        uc = UdpClient(protocol_description=fake_json, port=UDP_PORT, drone_ip="127.0.0.1")
         data = uc.get_data_dict()
         us.stop_thread()
         self.assertEqual(data, {'u1-2-v': 2, 'u1-2-t': 2, 'i1-2': 2})
